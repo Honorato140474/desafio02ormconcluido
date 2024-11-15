7d52acb98757567f5b3eca9c2ea9f24e435d48c1
Honorato Fernandes <honoratoffilho@gmail.com>
2024-11-15 18:10:41 -0300

    Initial commit

---
+++ src/main/java/br/com/honoratofernandes/desafio02orm/Desafio02OrmApplication.java
@@ -0,0 +1,13 @@
+package br.com.honoratofernandes.desafio02orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio02OrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio02OrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/honoratofernandes/desafio02orm/Desafio02OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.honoratofernandes.desafio02orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio02OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
